fe0cfb6d5f82a840fd85521c30980c8ddc244afa
Phoenix181 <40687700+Phoenix181@users.noreply.github.com>
2019-12-09 00:09:21 +0530

    Added some features in the game
    Now the selected item in move list is highlighted when selected but there are still some problems in it and the tokens which brought win to a player is highlighted now

--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 /*
 TODO: 1.[X] Display the location for each move in the format (col, row) in the move history list.
-      2.[-] Bold the currently selected item in the move list.
+      2.[X] Bold the currently selected item in the move list.
       3.[X] Rewrite Board to use two loops to make the squares instead of hardcoding them.
       4.[-] Add a toggle button that lets you sort the moves in either ascending or descending order.
-      5.[-] When someone wins, highlight the three squares that caused the win.
+      5.[X] When someone wins, highlight the three squares that caused the win.
       6.[-] When no one wins, display a message about the result being a draw.
 */
 
 function Square(props) {
   return(
-    <button className = "square" onClick = {props.onClick}>
+    <button className = {props.c} onClick = {props.onClick}>
       {props.value}
     </button>
   )
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, c) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
-      />
+        c={c} //class
+        />
     );
   }
 
   render() {
     const row = 3,
       total = 9;
+    const winningRow = this.props.winningRow;
     const boardElements = [];
-    for (let i = 0; i < total; i+=row) {
+    for (let i = 0; i < total; ) {
       const boardRow = [];
-      for (let j = 0; j < row; j++) {
-        boardRow.push(this.renderSquare(i));
+      for (let j = 0; j < row; j++, i++) {
+        if(winningRow.includes(i))
+          boardRow.push(this.renderSquare(i, "winner square"));
+        else
+          boardRow.push(this.renderSquare(i, "square"));
       }
       boardElements.push(<div className="board-row">{boardRow}</div>);
     }
@@ -49,6 +54,23 @@ class Board extends React.Component {
   }
 }
 
+function MoveButton(props) {
+  const [clicked, setClicked] = useState(false);
+  if(props.clicked && clicked)
+    setClicked(!clicked);
+  return (
+    <button
+      onClick={() => {
+        props.onClick();
+        setClicked(!clicked);
+      }}
+      className={clicked ? "moveClicked" : "move"}
+    >
+      {props.desc}
+    </button>
+  );
+}
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -57,11 +79,12 @@ class Game extends React.Component {
         {
           squares: Array(9).fill(null),
           row: null,
-          column: null,
+          column: null
         }
       ],
       stepNumber: 0,
       xIsNext: true,
+      clicked: false,
     };
   }
 
@@ -69,57 +92,68 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    const row = Math.floor(i/3+1);
-    const column = i%3+1;
-    if (calculateWinner(squares) || squares[i]) {
+    const row = Math.floor(i / 3 + 1);
+    const column = (i % 3) + 1;
+    if (calculateWinner(squares).winner || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      history: history.concat([{
-        squares: squares,
-        column: column,
-        row: row,
-      }]),
+      history: history.concat([
+        {
+          squares: squares,
+          column: column,
+          row: row
+        }
+      ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      clicked: true,
     });
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: step % 2 === 0
     });
   }
-
+  
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const { winner, winningRow } = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ?
-        `Go to move #${move}. In the column: ${step.column} and row: ${step.row}` :
-        'Go to game start';
+      const desc = move
+        ? `Go to move #${move}. In the column: ${step.column} and row: ${step.row}`
+        : "Go to game start";
       return (
         <li key={move}>
-          <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+          <MoveButton onClick={() => this.jumpTo(move)} desc={desc} clicked={this.state.clicked} />
         </li>
       );
     });
 
+    if(this.state.clicked) {
+      this.setState({
+        clicked: false,
+      });
+    }
     let status;
     if (winner) {
       status = `Winner ${winner}`;
-    }
-    else {
+    } else {
       status = `Next Player ${this.state.xIsNext ? "X" : "O"}`;
     }
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+            winningRow={winningRow}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
@@ -144,10 +178,16 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return ({
+        winner: squares[a],
+        winningRow: [a, b, c],
+      });
     }
   }
-  return null;
+  return ({
+    winner: null,
+    winningRow: [],
+  });
 }
 
 // ========================================
